9b2ab65342adc9925526fd8b16f6e458715a7c6c
codeleaded <gnu.c.reaper@gmail.com>
2026-07-24 01:39:18 +0200

    update

--- src/Main.c
+++ src/Main.c
@@ -1,11 +1,11 @@
 #if defined(__linux__) && !defined(_WIN32)
     #include "/home/codeleaded/System/Static/Library/OMML.h"
-    #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+    #include "/home/codeleaded/System/Static/Library/WindowEngine.h"
 #elif defined(_WIN32) || defined(_WIN64)
     #include "/home/codeleaded/System/Static/Library/OMML.h"
-    #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+    #include "/home/codeleaded/System/Static/Library/WindowEngine.h"
     //#include "F:/home/codeleaded/System/Static/Library/OMML.h"
-    //#include "F:/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+    //#include "F:/home/codeleaded/System/Static/Library/WindowEngine.h"
 #elif defined(__APPLE__)
     #error "Apple not supported!"
 #else
